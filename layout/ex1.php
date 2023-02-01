4d5ca93b81f9b459d431d18fbe87247555a51e33
Zara1365 <114822777+Zara1365@users.noreply.github.com>
2023-02-01 21:56:00 +0200

    the end of task 2

--- layout/ex1.php
+++ layout/ex1.php
@@ -1,25 +1,25 @@
 <?php include "header.php" ?>
-    <h1> 3.1 Write a simple PHP script to print your information (Name and your groupid)</h1>
+    <h1> 3.1) Write a simple PHP script to print your information (Name and your groupid)</h1>
 <?php
 echo "zahra hamidi <br>";
 echo "group 17";
 
 
 ?>
-<h1> 3.2 Write PHP code to display the following message.</h1>
+<h1> 3.2) Write PHP code to display the following message.</h1>
 <?php
 echo "Hello world ! My name is  \"David\"";
 ?>
 <h1>current Date</h1>
 <?php  echo date ("d.m.Y") ?>
 
-<h3> 3.4 variable in php </h3>
+<h3> 3.4)variable in php </h3>
 <?php
 $title1 = "php is intersting";
 
 echo "<h1>" . $title . " </h1>";
 ?>
-<h3> 3.5 table & variables</h3>
+<h3> 3.5) table & variables</h3>
 <?php
 $g1 =5;
 $g2 =4;
@@ -30,19 +30,19 @@ echo "
 <th> s.N. </th><th> Name </th><th> Grade</th>
 </tr>
 <tr>
-<td> 1 </td><td> pekka </td><td> $g1 </td>
+<td> 1 </td><td> Pekka </td><td> $g1 </td>
 </tr>
 <tr>
-<td> 1 </td><td> johanna </td><td> $g2 </td>
+<td> 2 </td><td> Johanna </td><td> $g2 </td>
 </tr>
 <tr>
-<td> 1 </td><td> john </td><td> $g3 </td>
+<td> 2 </td><td> John </td><td> $g3 </td>
 </tr>
 </table>
 ";
 ?>
 
-<h3> 4 Screenshot of the development environment</h3>
+<h3> 4) Screenshot of the development environment</h3>
 <img src="picture.png" alt="my picture">
 
 <?php include "footer.php" ?>
