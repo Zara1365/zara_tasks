2386b68edadccbd5cb15079a869fda922c4072a6
Zara1365 <114822777+Zara1365@users.noreply.github.com>
2023-02-09 22:40:54 +0200

    finished 3 b

--- layout/array.php
+++ layout/array.php
@@ -38,26 +38,27 @@ echo "</ul>";
 
 <?php
 
-$courses3=array("PHP", "HTML", "JavaScript", "CMS", "Project");
+$courses3=array(1=> "PHP", 2=> "HTML",3=> "JavaScript", 4=>"CMS",5=> "Project");
 asort($courses3);
-echo "<ul>";
-foreach ($courses as $c)
+
+foreach ($courses3 as $c)
 {
-    echo implode( [",$c "]);
+echo "$c, ";
 }
-echo "</ul>";
+
 ?>
 
+<h2>3)b)ascending order sort by Key  </h2>
 
-
-
-
-
- 
-
-
-
-
+<?php
+  $courses3=array("PHP", "HTML", "JavaScript", "CMS", "Project");
+ksort($courses3);  
+foreach( $courses3 as $c )    
+{    
+  echo "$<br />";    
+}    
+?>
+<h2>3)c)ascending order sort by Key  </h2>
 
 
 
