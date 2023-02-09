11411fc3397bf4a0a099714f4d8e04efbd1d4f19
Zara1365 <114822777+Zara1365@users.noreply.github.com>
2023-02-09 19:08:06 +0200

    finished 2

--- layout/array.php
+++ layout/array.php
@@ -18,6 +18,22 @@ echo "</ul>";
 <h2>2)The unset() function is used to remove element from the array</h2>
 
 
+<?php
+$courses1 = array(1=> "PHP", 2=> "HTML",3=> "JavaScript", 4=>"CMS",5=> "Project");
+foreach ($courses1 as $c)
+unset($courses1["3"]); 
+{
+echo "$c, ";
+}
+sort($courses1);
+echo "<ul>";
+foreach ($courses1 as $c)
+{
+echo "<li>$c</li>";
+}
+echo "</ul>";
+
+?>
 
 
 
